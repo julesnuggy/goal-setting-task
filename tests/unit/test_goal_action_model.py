94960dc64670d976c9eeeb10384399579a0dbfff
Julian Ng <julesnuggy@gmail.com>
2023-12-13 13:57:22 +0000

    TASK-1: Update tests for mark_as_complete

--- tests/unit/test_goal_action_model.py
+++ tests/unit/test_goal_action_model.py
@@ -48,6 +48,23 @@ def test_it_marks_sub_actions_as_complete():
     assert sub_action_2.completed is not None
 
 
+def test_it_marks_parent_action_as_complete_when_all_sub_actions_are_complete():
+    # arrange
+    goal = _create_goal()
+    action_1 = goal.create_action("SomeAction")
+    sub_action_1 = action_1.create_subaction("SomeSubAction1")
+    sub_action_2 = action_1.create_subaction("SomeSubAction2")
+
+    # act
+    sub_action_1.mark_as_complete()
+    sub_action_2.mark_as_complete()
+
+    # assert
+    assert action_1.completed is not None
+    assert sub_action_1.completed is not None
+    assert sub_action_2.completed is not None
+
+
 # TODO - 1
 def test_it_marks_as_not_complete():
     pass

25b7265653a1ad92b30201ca6b06a90fe302d169
Julian Ng <julesnuggy@gmail.com>
2023-12-13 13:54:11 +0000

    TASK-1: Update tests for mark_as_complete

--- tests/unit/test_goal_action_model.py
+++ tests/unit/test_goal_action_model.py
@@ -21,7 +21,31 @@ def test_it_creates_subaction():
 
 # TODO - 1
 def test_it_marks_as_complete():
-    pass
+    # arrange
+    goal = _create_goal()
+    action_1 = goal.create_action("SomeAction")
+
+    # act
+    action_1.mark_as_complete()
+
+    # assert
+    assert action_1.completed is not None
+
+
+def test_it_marks_sub_actions_as_complete():
+    # arrange
+    goal = _create_goal()
+    action_1 = goal.create_action("SomeAction")
+    sub_action_1 = action_1.create_subaction("SomeSubAction1")
+    sub_action_2 = action_1.create_subaction("SomeSubAction2")
+
+    # act
+    action_1.mark_as_complete()
+
+    # assert
+    assert action_1.completed is not None
+    assert sub_action_1.completed is not None
+    assert sub_action_2.completed is not None
 
 
 # TODO - 1
